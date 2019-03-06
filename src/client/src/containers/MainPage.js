855f7989a5ddd2bee295da31b98b4d9f0825c24d
Niklas Hedlund <nojan1989@gmail.com>
2019-03-06 14:00:34 +0100

    overflow hidden

--- src/client/src/containers/MainPage.js
+++ src/client/src/containers/MainPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import {connect} from 'react-redux';
-import { withRouter } from 'react-router';
+import { withRouter } from 'react-router-dom';
 
 import routes from '../constants/routes';
 
